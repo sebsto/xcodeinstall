c0a0a3823794b47d2307490be3a44ec37efcfcaf
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-09-06 12:47:59 +0200

    add shasum

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,3 +14,9 @@ git commit -m "release binaries $VERSION"
 git tag -a "v"$VERSION -m "Version $VERSION"
 git push origin "v"$VERSION
 
+wget https://github.com/sebsto/xcodeinstall/archive/refs/tags/v$VERSION.tar.gz
+shasum -a 256 v$VERSION.tar.gz
+rm v$VERSION.tar.gz
+
+# TODO : update brew Formula 
+
