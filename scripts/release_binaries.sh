736a29ec9da9ed7be06230ea0f7b825c9e44b45e
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-09-11 15:48:43 +0200

    add script to release binaries

--- scripts/release_binaries.sh
+++ scripts/release_binaries.sh
@@ -1,14 +1,36 @@
-# echo 
-# gh release create "v"$VERSION --generate-notes
-# gh release upload "v"$VERSION bottle files
+#!/bin/sh -e
 
-# git add dist/*
-# git commit -m "release binaries $VERSION"
-# git tag -a "v"$VERSION -m "Version $VERSION"
-# git push origin "v"$VERSION
+echo "\n➕ Get version number \n"
+if [ ! -f VERSION ]; then 
+    echo "VERSION file does not exist."
+    echo "It is created by 'scripts/release_sources.sh"
+    exit -1
+fi
+VERSION=$(cat VERSION)
+TAG=v$VERSION
 
-# wget https://github.com/sebsto/xcodeinstall/archive/refs/tags/v$VERSION.tar.gz
-# shasum -a 256 v$VERSION.tar.gz
-# rm v$VERSION.tar.gz
+echo "⬆️ Upload bottles"
+gh release upload $TAG dist/bottle/*
 
-# TODO : update brew Formula 
+echo "🍺 Add bottles to brew formula"
+if [ ! -f ./scripts/xcodeinstall.rb ]; then 
+    echo "Brew formula file does not exist. (./scripts/xcodeinstall.rb)"
+    echo "It is created by 'scripts/release_sources.sh"
+    exit -1
+fi
+echo "🍺 Add bottles to brew formula"
+if [ ! -f ./BOTTLE_BLOCK ]; then 
+    echo "Bottle block file does not exist. (./BOTTLE_BLOCK)"
+    echo "It is created by 'scripts/bottle.sh"
+    exit -1
+fi
+sed -i .bak -E -e "/  # insert bottle definition here/r BOTTLE_BLOCK" ./scripts/xcodeinstall.rb
+rm ./scripts/xcodeinstall.rb.bak
+
+echo "\n🍺 Pushing new formula\n"
+cp scripts/xcodeinstall.rb ../homebrew-macos
+pushd ../homebrew-macos
+git add xcodeinstall.rb 
+git commit --quiet -m "update for $TAG"
+git push --quiet > /dev/null 2>&1
+popd 
