1ccbad3e6f48116b06fe022755292a613c9ff377
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-09-26 11:46:43 +0200

    move display and readline to CLIlib

--- Sources/xcodeinstall/CLI/XcodeInstallCommand.swift
+++ Sources/xcodeinstall/CLI/XcodeInstallCommand.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CLIlib
 import Logging
 
 struct XCodeInstall {
@@ -23,23 +24,6 @@ struct XCodeInstall {
         display.display(msg, terminator: terminator)
     }
 
-    struct ReadLine: ReadLineProtocol {
-        func readLine(prompt: String, silent: Bool = false) -> String? {
-            if silent {
-                return String(cString: getpass(prompt))
-            } else {
-                Display().display(prompt, terminator: "")
-                return Swift.readLine()
-            }
-        }
-    }
-
-    struct Display: DisplayProtocol {
-        func display(_ msg: String, terminator: String) {
-            print(msg, terminator: terminator)
-        }
-    }
-
     // specialised classes to implement functionalities
     // allows to inject other implementations for testing
     // declared here because extension can not hold variables
@@ -50,14 +34,6 @@ struct XCodeInstall {
     var secretsManager: SecretsHandler
     var logger: Logger
     var fileHandler: FileHandlerProtocol
-}
-
-protocol ReadLineProtocol {
-    func readLine(prompt: String, silent: Bool) -> String?
-}
-
-protocol DisplayProtocol {
-    func display(_ msg: String, terminator: String)
 }
 
 enum XCodeInstallError: Error {
--- Tests/xcodeinstallTests/CLI/CLITest.swift
+++ Tests/xcodeinstallTests/CLI/CLITest.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import ArgumentParser
+import CLIlib
 @testable import xcodeinstall
 
 
