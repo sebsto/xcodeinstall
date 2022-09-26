1863b249c025c03b723348422e80728f827697e0
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-09-26 11:25:28 +0200

    add test for Soto

---
+++ Tests/xcodeinstallTests/Secrets/AWSSecretsHandlerSotoTest.swift
@@ -0,0 +1,117 @@
+//
+//  AWSSecretsHandlerSotoTest.swift
+//  xcodeinstallTests
+//
+//  Created by Stormacq, Sebastien on 16/09/2022.
+//
+
+import XCTest
+import SotoCore
+import SotoSecretsManager
+
+@testable import xcodeinstall
+
+final class AWSSecretsHandlerSotoTest: XCTestCase {
+    
+    static var awsClient : AWSClient!
+    static var smClient  : SecretsManager!
+    var log : Log?
+    var secretHandler : AWSSecretsHandlerSoto?
+    
+    override func setUpWithError() throws {
+        // given
+        let region = "us-east-1"
+        
+        // when
+        do {
+            Self.awsClient = AWSClient(credentialProvider: TestEnvironment.credentialProvider,
+                                       middlewares: TestEnvironment.middlewares,
+                                       httpClientProvider: .createNew)
+            Self.smClient = SecretsManager(client: AWSSecretsHandlerSotoTest.awsClient,
+                                           endpoint: TestEnvironment.getEndPoint())
+            
+            log = Log(logLevel: .debug)
+
+            secretHandler = try AWSSecretsHandlerSoto(region: region, logger: log!.defaultLogger)
+            XCTAssertNotNil(secretHandler)
+            XCTAssertNoThrow(try secretHandler!.awsClient.syncShutdown()) // shut down the class provided AWS Client
+
+            // replace our clients for testing (requires localstack to run)
+            secretHandler!.awsClient = AWSSecretsHandlerSotoTest.awsClient
+            secretHandler!.smClient = AWSSecretsHandlerSotoTest.smClient
+
+            if TestEnvironment.isUsingLocalstack {
+                print("Connecting to Localstack")
+            } else {
+                print("Connecting to AWS")
+            }
+            
+            // then
+            // no error
+            
+        } catch AWSSecretsHandlerError.invalidRegion(let error) {
+            XCTAssertEqual(region, error)
+        } catch {
+            XCTAssert(false, "unexpected error : \(error)")
+        }
+
+    }
+    
+    override func tearDownWithError() throws {
+        XCTAssertNoThrow(try Self.awsClient!.syncShutdown())
+    }
+    
+    func testInitWithCorrectRegion() {
+        
+        // given
+        let region = "us-east-1"
+        
+        // when
+        do {
+            let _ = try AWSSecretsHandlerSoto(region: region, logger: log!.defaultLogger)
+            
+            // then
+            // no error
+            
+        } catch AWSSecretsHandlerError.invalidRegion(let error) {
+            XCTAssert(false, "region rejected : \(error)")
+        } catch {
+            XCTAssert(false, "unexpected error : \(error)")
+        }
+    }
+    
+    func testInitWithIncorrectRegion() {
+        
+        // given
+        let region = "invalid"
+        
+        // when
+        do {
+            let _ = try AWSSecretsHandlerSoto(region: region, logger: log!.defaultLogger)
+            
+            // then
+            // error
+            XCTAssert(false, "an error must be thrown")
+            
+        } catch AWSSecretsHandlerError.invalidRegion(let error) {
+            XCTAssertEqual(region, error)
+        } catch {
+            XCTAssert(false, "unexpected error : \(error)")
+        }
+    }
+    
+    func testCreateSecret() async {
+        
+        // given
+        XCTAssertNotNil(secretHandler)
+        let credentials = AppleCredentialsSecret(username: "username", password: "password")
+
+        // when
+        do {
+          try await secretHandler!.updateSecret(secretId: .appleCredentials, newValue: credentials)
+        } catch {
+            XCTAssert(false, "unexpected error : \(error)")
+        }
+        
+    }
+}
---
+++ Tests/xcodeinstallTests/Secrets/SotoTestEnvironment.swift
@@ -0,0 +1,53 @@
+//
+//  SotoTestEnvironment.swift
+//  xcodeinstallTests
+//
+//  Created by Stormacq, Sebastien on 16/09/2022.
+//
+
+import Dispatch
+import Foundation
+import SotoCore
+import XCTest
+
+/// Provide various test environment variables
+enum TestEnvironment {
+    /// are we using Localstack to test. Also return use localstack if we are running a github action and don't have an access key if
+    static var isUsingLocalstack: Bool {
+//        return Environment["AWS_DISABLE_LOCALSTACK"] != "true" ||
+//            (Environment["GITHUB_ACTIONS"] == "true" && Environment["AWS_ACCESS_KEY_ID"] == "")
+        return true
+    }
+
+    static var credentialProvider: CredentialProviderFactory { return isUsingLocalstack ? .static(accessKeyId: "foo", secretAccessKey: "bar") : .default }
+
+    /// current list of middleware
+    static var middlewares: [AWSServiceMiddleware] {
+//        return (Environment["AWS_ENABLE_LOGGING"] == "true") ? [AWSLoggingMiddleware()] : []
+        return []
+    }
+
+    /// return endpoint
+    static func getEndPoint(environment: String = "") -> String? {
+        guard self.isUsingLocalstack == true else { return nil }
+//        return Environment[environment] ?? "http://localhost:4566"
+        return "http://localhost:4566"
+    }
+
+    /// get name to use for AWS resource
+//    static func generateResourceName(_ function: String = #function) -> String {
+//        let prefix = Environment["AWS_TEST_RESOURCE_PREFIX"] ?? ""
+//        return "soto-" + (prefix + function).filter { $0.isLetter || $0.isNumber }.lowercased()
+//    }
+
+//    public static var logger: Logger = {
+//        if let loggingLevel = Environment["AWS_LOG_LEVEL"] {
+//            if let logLevel = Logger.Level(rawValue: loggingLevel.lowercased()) {
+//                var logger = Logger(label: "soto")
+//                logger.logLevel = logLevel
+//                return logger
+//            }
+//        }
+//        return AWSClient.loggingDisabled
+//    }()
+}
